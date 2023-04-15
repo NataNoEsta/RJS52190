61f4b794e79442511c8e4cb8ce4ad0de253afb42
nataberger <natasha.berger@innew.la>
2023-04-15 11:38:13 -0300

    primer commit

---
+++ clase-03/myshop/src/components/TarjetaPersonal.jsx
@@ -0,0 +1,11 @@
+const TarjetaPersonal = (props) => {
+    console.log(props)
+    return (
+        <div>
+            <h1>Natasha Berger</h1>
+            <p>Edad: 31</p>
+            <p>Curso: React JS</p>
+        </div>
+    )
+}
+export default TarjetaPersonal
---
+++ proyecto/mamivintage/src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+const Navbar = () => {
+	return (
+		<nav className="navbar navbar-expand-lg bg-light">
+			<div className="container-fluid">
+				<a className="navbar-brand" href="#">
+					Mami Vintage
+				</a>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarNavAltMarkup"
+					aria-controls="navbarNavAltMarkup"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+					<div className="navbar-nav">
+						<a className="nav-link active" aria-current="page" href="#">
+							Inicio
+						</a>
+						<a className="nav-link" href="#">
+							Features
+						</a>
+						<a className="nav-link" href="#">
+							Pricing
+						</a>
+					</div>
+				</div>
+			</div>
+		</nav>
+	);
+};
+export default Navbar;
