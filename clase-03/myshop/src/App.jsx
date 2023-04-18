d23e22124d9716eb0118a9d98c1daec3689c87e2
nataberger <natasha.berger@innew.la>
2023-04-18 07:09:57 -0300

    prácticas de clase

--- clase-03/myshop/src/App.jsx
+++ clase-03/myshop/src/App.jsx
@@ -5,13 +5,12 @@ import  TarjetaPersonal  from './components/TarjetaPersonal'
 function App() {
 
   return (
-    <div className="App">
-    
-      <h1>Hola, mundo!</h1>
-      <Equipo />
-      <TarjetaPersonal />
-    </div>
-  )
+		<div className="App">
+			<h1>Hola, mundo!</h1>
+			<Equipo />
+			<TarjetaPersonal nombre="Natasha Berger" edad="31" curso="React JS" />
+		</div>
+  );
 }
 
 export default App
