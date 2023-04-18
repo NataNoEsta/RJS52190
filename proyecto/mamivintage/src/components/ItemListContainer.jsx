4f678d0ce6ab6f3313ee7826ab65946188fd3b6f
nataberger <natasha.berger@innew.la>
2023-04-18 07:10:56 -0300

    Agregados navbar y cart

---
+++ proyecto/mamivintage/src/components/ItemListContainer.jsx
@@ -0,0 +1,7 @@
+export const ItemListContainer = (props) => {
+	return (
+		<ul>
+			<li>Hola, {props.name}!</li>
+		</ul>
+	);
+};
--- proyecto/mamivintage/src/components/Navbar.jsx
+++ proyecto/mamivintage/src/components/Navbar.jsx
@@ -1,36 +1,44 @@
+import "./navbar.css";
+
 const Navbar = () => {
 	return (
-		<nav className="navbar navbar-expand-lg bg-light">
-			<div className="container-fluid">
-				<a className="navbar-brand" href="#">
-					Mami Vintage
-				</a>
-				<button
-					className="navbar-toggler"
-					type="button"
-					data-bs-toggle="collapse"
-					data-bs-target="#navbarNavAltMarkup"
-					aria-controls="navbarNavAltMarkup"
-					aria-expanded="false"
-					aria-label="Toggle navigation"
-				>
-					<span className="navbar-toggler-icon"></span>
-				</button>
-				<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-					<div className="navbar-nav">
-						<a className="nav-link active" aria-current="page" href="#">
-							Inicio
-						</a>
-						<a className="nav-link" href="#">
-							Features
-						</a>
-						<a className="nav-link" href="#">
-							Pricing
-						</a>
+		<>
+			<nav className="navbar navbar-expand-lg bg-light">
+				<div className="container-fluid">
+					<a className="navbar-brand" href="#">
+						Mami Vintage
+					</a>
+					<button
+						className="navbar-toggler"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#navbarNavAltMarkup"
+					>
+						<span className="navbar-toggler-icon"></span>
+					</button>
+					<div
+						className="collapse navbar-collapse"
+						id="navbarNavAltMarkup"
+					>
+						<div className="navbar-nav">
+							<a
+								className="nav-link active"
+								aria-current="page"
+								href="/"
+							>
+								Shop
+							</a>
+							<a className="nav-link" href="#">
+								Nosotros
+							</a>
+							<a className="nav-link" href="#">
+								FAQ
+							</a>
+						</div>
 					</div>
 				</div>
-			</div>
-		</nav>
+			</nav>
+		</>
 	);
 };
 export default Navbar;
