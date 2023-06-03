3c57b42fb0ac3e56ad0f22d7c5f5d7f8e9ad1f90
nataberger <natasha.berger@innew.la>
2023-06-03 15:43:12 -0300

    clase01

---
+++ clase01/pruebas/src/App.jsx
@@ -0,0 +1,13 @@
+import AppTest from "./components/AppTest"
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+
+
+  return (
+    <>
+      <AppTest/>
+    </>
+  )
+}
+
+export default App
